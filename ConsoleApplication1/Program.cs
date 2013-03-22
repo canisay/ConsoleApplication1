f3f92ee1fe75820fd46529a9163b1b19aca0be9a
Richard Thompson <richard.thompson@bedegaming.com>
2013-03-22 16:22:20 +0000

    first commit

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+ 
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+            string connectionProperties = "Server=RICH-MAC\\SQLEXPRESS;Database=testdb;Trusted_Connection=true;";
+
+            SqlConnection myDbConnection = new SqlConnection( connectionProperties );
+
+            connect( myDbConnection );
+
+            SqlDataReader myReader = null;
+            SqlCommand myCommand = new SqlCommand( "SELECT * FROM test_tbl", myDbConnection );
+            myReader = myCommand.ExecuteReader();
+            while (myReader.Read())
+            {
+                Console.WriteLine(myReader["id"].ToString());
+            }
+        }
+
+        private static void connect( SqlConnection connection )
+        {
+            Console.WriteLine("called"); 
+            try
+            {
+                Console.WriteLine("try"); 
+                connection.Open();
+                Console.WriteLine("try2"); 
+            }
+            catch( Exception e )
+            {
+                Console.WriteLine("exception caught"); 
+                Console.WriteLine(e.ToString());
+                Environment.Exit(0);
+            }
+        }
+
+    }
+}
